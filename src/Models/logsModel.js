5a5120d259df08d16a994aaca772af6dfa06adde
XanderEsparza <2136000805@utna.edu.mx>
2024-08-08 17:32:53 -0600

    Creacion del modelo para logs

---
+++ src/Models/logsModel.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../conectionDB')
+
+const Log = sequelize.define('Log', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    level: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    message: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    usuario: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    rol: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    fecha_hora: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+}, {
+    tableName: 'logs',
+    timestamps: false
+});
+
+module.exports = Log;
