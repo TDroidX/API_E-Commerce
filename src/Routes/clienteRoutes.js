b36f92c907916db5bfc939f65c2255aac92b12a2
TDroidX <pizanadesantiagoricardo@gmail.com>
2024-08-16 11:07:02 -0600

    Se corrigio error de rol

--- src/Routes/clienteRoutes.js
+++ src/Routes/clienteRoutes.js
@@ -12,7 +12,7 @@ const checkRole = require('../middlewares/checkRole')
 // router.get('/create', clienteController.create)
 router.post('/store', clienteController.store);
 router.get('/show', checkAuth, checkRole(['administrador']), clienteController.show);
-router.get('/find/:id', checkAuth, checkRole(['adminsitrador']), clienteController.find);
+router.get('/find/:id', checkAuth, checkRole(['administrador']), clienteController.find);
 // router.get('/edit/:id', clienteController.edit)
 router.put('/update/:id', checkAuth, clienteController.update);
 router.delete('/destroy/:id', checkAuth, checkRole(['administrador']), clienteController.destroy);
