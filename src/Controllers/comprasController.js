eaa48e133703b6f46ffb8466479c2e5c14c87379
XanderEsparza <2136000805@utna.edu.mx>
2024-08-16 07:48:58 -0600

    Se agregó la función stripe

--- src/Controllers/comprasController.js
+++ src/Controllers/comprasController.js
@@ -1,4 +1,8 @@
 const Compras = require('../Models/comprasModel.js');
+const Carrito = require('../Models/carritoModel.js')
+const Producto = require('../Models/productosModel.js')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const { Op } = require('sequelize');
 
 // Obtener todas las compras de un usuario por IDCliente
@@ -37,3 +41,51 @@ exports.obtenerCompraPorUsuarioYID = async (req, res) => {
         res.status(500).json({ error: 'Error al obtener la compra del usuario.' });
     }
 };
+
+//STRIPE
+
+exports.crearSesionDePago = async (req, res) => {
+    const idCliente = req.id;
+
+    try {
+        // Obtener los items del carrito
+        const itemsCarrito = await Carrito.findAll({
+            where: { IDCliente: idCliente },
+            raw: true
+        });
+
+        if (itemsCarrito.length === 0) {
+            return res.status(400).json({ error: 'El carrito está vacío.' });
+        }
+
+        // Crear los objetos de línea para Stripe
+        const lineItems = itemsCarrito.map(item => ({
+            price_data: {
+                currency: 'mxn', // Cambia la moneda según tu configuración
+                product_data: {
+                    name: item.NombreProducto
+                },
+                unit_amount: parseInt(item.PrecioUnitario * 100) // Stripe usa centavos
+            },
+            quantity: item.CantidadProductos
+        }));
+
+        // Crear la sesión de pago en Stripe
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: `http://localhost:3000/api/carrito/finCompra`,
+            cancel_url: `http://localhost:3000/api/compras/cancel`,
+        });
+
+        // Devolver el ID de la sesión de pago
+        res.json({ url: session.url });
+    } catch (error) {
+        console.error('Error al crear sesión de pago:', error);
+        res.status(500).json({ error: 'Error al crear la sesión de pago.' });
+    }
+};
+exports.cancel = (req, res) => {
+    res.status(200).json({message: 'El pago ha sido cancelado'})
+}
